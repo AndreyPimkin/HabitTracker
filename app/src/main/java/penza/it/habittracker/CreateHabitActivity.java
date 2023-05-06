b393730d006577036975e3bd4374e08b56188892
AndreyPimkin <pimkin03@bk.ru>
2023-05-06 17:32:07 +0300

    Окно создания привычек  (начало), исправил еще некоторые моменты

---
+++ app/src/main/java/penza/it/habittracker/CreateHabitActivity.java
@@ -0,0 +1,14 @@
+package penza.it.habittracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateHabitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_habit);
+    }
+}
--- app/src/main/java/penza/it/habittracker/SecondActivity.java
+++ app/src/main/java/penza/it/habittracker/SecondActivity.java
@@ -1,8 +1,10 @@
 package penza.it.habittracker;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,12 +14,23 @@ import androidx.fragment.app.FragmentTransaction;
 public class SecondActivity extends AppCompatActivity {
 
     private FrameLayout frameLayout;
-
+    HabitFragment habitFragment =  new HabitFragment();
+    private TextView textViewHabit, textViewHistory, textViewPop, textViewPerson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        textViewHabit = findViewById(R.id.textHabit);
+        textViewHistory = findViewById(R.id.textHistory);
+        textViewPop = findViewById(R.id.textPop);
+        textViewPerson = findViewById(R.id.textPerson);
+
+        textViewHabit.setTextColor(Color.parseColor("#c8c8c8"));
+
+        habitFragment =  new HabitFragment();
+        setNewFragment(habitFragment);
 
         Bundle arguments = getIntent().getExtras();
          if(arguments != null){
@@ -35,11 +48,17 @@ public class SecondActivity extends AppCompatActivity {
     }
 
     public void openHabit(View v) {
-        HabitFragment habitFragment =  new HabitFragment();
+        habitFragment =  new HabitFragment();
         setNewFragment(habitFragment);
     }
 
     private void setNewFragment(Fragment fragment) {
+        textViewHabit.setTextColor(Color.parseColor("#c8c8c8"));
+        textViewHistory.setTextColor(Color.parseColor("#ffffff"));
+        textViewPop.setTextColor(Color.parseColor("#ffffff"));
+        textViewPerson.setTextColor(Color.parseColor("#ffffff"));
+
+
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.frameMain, fragment);
         ft.addToBackStack(null);
@@ -47,14 +66,27 @@ public class SecondActivity extends AppCompatActivity {
     }
 
     public void openHistory(View v) {
+        textViewHabit.setTextColor(Color.parseColor("#ffffff"));
+        textViewHistory.setTextColor(Color.parseColor("#c8c8c8"));
+        textViewPop.setTextColor(Color.parseColor("#ffffff"));
+        textViewPerson.setTextColor(Color.parseColor("#ffffff"));
 
     }
 
     public void openPop(View v) {
 
+        textViewHabit.setTextColor(Color.parseColor("#ffffff"));
+        textViewHistory.setTextColor(Color.parseColor("#ffffff"));
+        textViewPop.setTextColor(Color.parseColor("#c8c8c8"));
+        textViewPerson.setTextColor(Color.parseColor("#ffffff"));
     }
 
     public void openPerson(View v) {
+
+        textViewHabit.setTextColor(Color.parseColor("#ffffff"));
+        textViewHistory.setTextColor(Color.parseColor("#ffffff"));
+        textViewPop.setTextColor(Color.parseColor("#ffffff"));
+        textViewPerson.setTextColor(Color.parseColor("#c8c8c8"));
 
     }
 
