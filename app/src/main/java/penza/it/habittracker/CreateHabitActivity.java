95905c04e6be443cc5defabce96a2e753c418a61
AndreyPimkin <pimkin03@bk.ru>
2023-05-08 18:27:01 +0300

    Дизайн окна создания привычек

--- app/src/main/java/penza/it/habittracker/CreateHabitActivity.java
+++ app/src/main/java/penza/it/habittracker/CreateHabitActivity.java
@@ -1,135 +1,30 @@
 package penza.it.habittracker;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
 public class CreateHabitActivity extends AppCompatActivity {
-
-    private ArrayList<String> categoryList = new ArrayList<>(10);
-    private ArrayList<String> descriptionList = new ArrayList<>(10);
-    private ArrayList<String> imageList = new ArrayList<>(10);
-    private ListView listView;
-    private CategoryAdapter categoryAdapter;
-    private DatabaseHelper mDBHelper;
-    private SQLiteDatabase mDb;
-    private Cursor cursor;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_habit);
-        mDBHelper = new DatabaseHelper(this);
-
-        try {
-            mDBHelper.updateDataBase();
-        } catch (IOException mIOException) {
-            throw new Error("UnableToUpdateDatabase");
-        }
-
-        try {
-            mDb = mDBHelper.getWritableDatabase();
-        } catch (SQLException mSQLException) {
-            throw mSQLException;
-        }
-
-        initList();
-
-        categoryAdapter = new CategoryAdapter(this);
-        listView = findViewById(R.id.listCategory);
-        listView.setAdapter(categoryAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @SuppressLint("RestrictedApi")
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(CreateHabitActivity.this, ChoiceHabitActivity.class);
-                intent.putExtra("name", categoryList.get(position));
-                intent.putExtra("description", descriptionList.get(position));
-                startActivity(intent);
-                finish();
-            }
-        });
-
     }
 
-    public void backWindow(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
-        finish();
+    public void matterChoice(View view) {
     }
 
-    // Пишем свой класс-адаптер
-    private class CategoryAdapter extends BaseAdapter {
-        private LayoutInflater mLayoutInflater;
-
-        CategoryAdapter(Context context) {
-            mLayoutInflater = LayoutInflater.from(context);
-        }
-
-        @Override
-        public int getCount() {
-            return categoryList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return position;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @SuppressLint("DiscouragedApi")
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null)
-                convertView = mLayoutInflater.inflate(R.layout.list_design, null);
-
-            ImageView image = (ImageView) convertView.findViewById(R.id.image_habit);
-            image.setImageResource(getResources().getIdentifier(imageList.get(position), "drawable", getPackageName()));
-
-            TextView signTextView = (TextView) convertView.findViewById(R.id.name_habit);
-            signTextView.setText(categoryList.get(position));
-
-            TextView dateTextView = (TextView) convertView.findViewById(R.id.description_habit);
-            dateTextView.setText(descriptionList.get(position));
-
-            return convertView;
-        }
+    public void createHabit(View view) {
     }
 
-    private void initList() {
-        cursor = mDb.rawQuery("SELECT * FROM categories", null);
-        if (cursor.moveToFirst()) {
-            while (!cursor.isAfterLast()) {
-                categoryList.add(cursor.getString(1));
-                descriptionList.add(cursor.getString(2));
-                imageList.add(cursor.getString(3));
-                cursor.moveToNext();
-            }
-        }
-        cursor.close();
+    public void morningChoice(View view) {
     }
 
+    public void eveningChoice(View view) {
+    }
+
+    public void backChoiceWindow(View view) {
+    }
 }
